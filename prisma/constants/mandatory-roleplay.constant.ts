d3a1f9648099753243db9111f7f1d7d1ca25df75
danielokpa <danielokpa769@gmail.com>
2026-08-07 12:43:50 +0100

    Update: Added required fields to mandatory roleplay bank seed constants

--- prisma/constants/mandatory-roleplay.constant.ts
+++ prisma/constants/mandatory-roleplay.constant.ts
@@ -1,4 +1,4 @@
-import { RolePlayCategory, JobRole } from '@prisma/client';
+import { RolePlayCategory, JobRole, RolePlayAnswerType } from '@prisma/client';
 
 export const MANDATORY_ROLEPLAY_QUESTIONS = [
   {
@@ -12,5 +12,8 @@ export const MANDATORY_ROLEPLAY_QUESTIONS = [
     jobRole: JobRole.DRIVER,
     prompt: 'Do you have an existing account with Uber/Bolt/inDrive? If yes, upload your account(Upload atleast two of these:\n1. Account Status: Showing active/approved status. \n2. Driver Rating: Total lifetime stars (e.g., 4.9 stars).\n3. Trip History: Lifetime number of completed trips.\n4. Acceptance/Cancellation Rates: Metric percentages of accepted or dropped rides.\n5. Profile Picture: Legal name alongside the approved clear selfie.).',
     isMandatory: true,
+    answerType: RolePlayAnswerType.TEXT_AND_FILE,
+    minFiles: 2,
+    maxFile: 5,
   },
 ]
