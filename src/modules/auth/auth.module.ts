f65ad5c5b218d2bedcb42b674d55c7c764439c94
danielokpa <danielokpa769@gmail.com>
2026-09-04 05:37:38 +0100

    Updates: import patients module in auth module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { AuthController } from './controllers/auth.controller';
 import { AuthService } from './auth.service';
 // import { CountriesModule } from '../countries/countries.module';
 import { UsersModule } from '../users/users.module';
+import { PatientsModule } from '../patients/patient.module';
 // import { ClientDevicesModule } from '../client-devices/client-devices.module';
 
 @Module({
@@ -13,6 +14,7 @@ import { UsersModule } from '../users/users.module';
     MailModule,
     // CountriesModule,
     UsersModule,
+    PatientsModule,
     // ClientDevicesModule,
   ],
   providers: [AuthService],
